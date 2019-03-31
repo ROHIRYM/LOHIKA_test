bff8f10595897b7986dc35c9d7b119ca729a2555
rohirym <igor.maiba@gmail.com>
2019-04-01 00:21:57 +0300

    Add getListOfCsvFiles method

--- src/main/scala/lohika/Main.scala
+++ src/main/scala/lohika/Main.scala
@@ -1,11 +1,28 @@
 package lohika
 
+import java.io.File
+
 object Main extends App {
     val areArgsValid = args.length == 2 && args(0) == "-d"
 
     if (!areArgsValid) {
         println("Invalid arguments")
     } else {
-        
+        val listOfCsvFiles = getListOfCsvFiles(args(1))
+        if (listOfCsvFiles.isEmpty) {
+            println("=== No .csv files found ===")
+        } else {
+            println("=== Found .csv files ===")
+            listOfCsvFiles.foreach{(f: File) => println(f.getName)}
+        }
+    }
+
+    def getListOfCsvFiles(dir: String): List[File] = {
+        val d = new File(dir)
+        if (d.exists && d.isDirectory) {
+            d.listFiles.filter(_.isFile).toList.filter(_.getName.endsWith(".csv"))
+        } else {
+            List[File]()
+        }
     }
 }
